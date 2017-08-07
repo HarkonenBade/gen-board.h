7bca753ed79ca013a9d3f4b3a5e0e2b4e3b835f1
Thomas Bytheway <tb403@cam.ac.uk>
2017-08-07 22:24:00 +0100

    Make MCU detection simpler

--- gbdh/mcu.py
+++ gbdh/mcu.py
@@ -6,24 +6,19 @@ MCU = collections.namedtuple('MCU', ['ports', 'pins_per_port'])
 
 
 MCU_LIST = {
-    'STM32F0xxxx': MCU(ports=['A', 'B', 'C', 'D', 'F'],
-                       pins_per_port=16),
-    'STM32F4xxxx': MCU(ports=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
-                       pins_per_port=16)
+    'STM32F0': MCU(ports=['A', 'B', 'C', 'D', 'F'],
+                   pins_per_port=16),
+    'STM32F4': MCU(ports=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
+                   pins_per_port=16)
 }
 
 
 def get_mcu(mcu_type):
     def match_names(test, x):
-        s = 0
-        for tc, xc in zip(test, x):
-            if tc == xc:
-                s += 1
-            elif x == "x":
-                continue
-            else:
-                s -= 1
-        return s
+        if test.upper().startswith(x.upper()):
+            return len(x)
+        else:
+            return -1
 
     rank_types = sorted(MCU_LIST.keys(),
                         key=lambda x: match_names(mcu_type, x),
